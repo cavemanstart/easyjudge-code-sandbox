cad48b0883876a2c205efd07124358c2443eac6b
stone <1265286368@qq.com>
2024-08-06 15:59:56 +0800

    添加docker file

--- src/main/java/com/stone/controller/MainController.java
+++ src/main/java/com/stone/controller/MainController.java
@@ -5,14 +5,14 @@ import com.stone.codesandbox.JavaDockerCodeSandbox;
 import com.stone.model.ExecuteCodeRequest;
 import com.stone.model.ExecuteCodeResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.annotation.Resource;
 
 @RestController("/")
 @Slf4j
@@ -42,7 +42,6 @@ public class MainController {
                                     HttpServletResponse response) {
         // 基本的认证
         String authHeader = request.getHeader(AUTH_REQUEST_HEADER);
-        log.info("test point");
         if (!AUTH_REQUEST_SECRET.equals(authHeader)) {
             response.setStatus(403);
             return null;
